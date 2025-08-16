aa7814b02e9c1096ed95a27b257b4b2cf7293722
Akshay Kulkarni <akshaykulkarni0502@gmail.com>
2025-08-16 09:37:15 +0530

    Refactor classA.java: improve code readability and maintainability

--- classA.java
+++ classA.java
@@ -3,5 +3,8 @@ public class classA {
         System.out.println("This is class A");
     }
     public void method1(){
-         System.out.println("This is class A method 1"); }
+        System.out.println("This is class A line 1");
+        System.out.println("This is class A line 2"); 
+
+        }
 }
